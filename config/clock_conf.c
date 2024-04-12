ad6e23fb5de5fda667cfe4a9e78fd60dc59e8993
gxl <2738933375@qq.com>
2024-04-13 03:11:39 +0800

    增加switch端口输出文件和时钟管理配置文件

---
+++ config/clock_conf.c
@@ -0,0 +1,8 @@
+#include "clock_conf.h"
+
+void vClockEnable(void)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
+}
---
+++ config/clock_conf.h
@@ -0,0 +1,6 @@
+#ifndef __CLOCK_CONF_H__
+#define __CLOCK_CONF_H__
+#include <stm32f10x.h>
+
+void vClockEnable(void);
+#endif
